d294881c01e936e3eb6203c33333e6049420d50e
Jeff Hensal <hensal.7@osu.edu>
2019-09-30 23:15:23 -0400

    initial commit, bare but functional client/server

---
+++ client.py
@@ -0,0 +1,16 @@
+# importing the requests library 
+import requests 
+
+# api-endpoint 
+URL = "http://127.0.0.1:5000/"
+
+# sending get request and saving the response as response object 
+r = requests.get(url = URL)
+
+if r == 200:
+    print('Success!')
+elif r == 404:
+    print('Not Found.')
+
+# printing the output 
+print("Response: %s"%r.text)
