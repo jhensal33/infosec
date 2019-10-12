b2d4288997e3bef24271f14a791105a00af6ad57
hyperwing <drdavidwing@gmail.com>
2019-10-12 10:46:03 -0400

    init import

--- client.py
+++ client.py
@@ -1,16 +1,34 @@
 # importing the requests library 
 import requests 
+import jwt
 
 # api-endpoint 
 URL = "http://127.0.0.1:5000/"
 
-# sending get request and saving the response as response object 
-r = requests.get(url = URL)
 
-if r == 200:
-    print('Success!')
-elif r == 404:
-    print('Not Found.')
+def authenticate_client_key():
 
-# printing the output 
-print("Response: %s"%r.text)
+    # Hit AD and retrieve JWT
+
+
+    # Decode key once received from AD 
+    jwt.decode(key, 'secret', algorithms=['HS256'])
+
+    # return credentials
+    return ""
+
+
+if __name__ == '__main__':
+
+    credentials = authenticate_client_key()
+
+    # sending get request and saving the response as response object 
+    r = requests.get(url = URL)
+
+    if r == 200:
+        print('Success!')
+    elif r == 404:
+        print('Not Found.')
+
+    # printing the output 
+    print("Response: %s"%r.text)
