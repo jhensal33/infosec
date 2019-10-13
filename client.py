1b3255346931184ed1c356bf88e7f5b061dfbb07
hyperwing <drdavidwing@gmail.com>
2019-10-13 16:51:29 -0400

    changed flow

--- client.py
+++ client.py
@@ -8,19 +8,31 @@ URL = "http://127.0.0.1:5000/"
 
 def authenticate_key():
 
-    # Hit AD and retrieve JWT
-    key = ""
+    # Receive JWT from server
+    public_key = ""
+
+    # encode message with public key 
+    message = "random"
+    encoded_message = jwt.encode(message, public_key, algorithms=['HS256'])
+    
+    # send encoded message to server
+    print(encoded_message)
 
     # Decode key once received from AD 
-    jwt.decode(key, 'secret', algorithms=['HS256'])
+    decoded_message = jwt.decode(public_key, 'secret', algorithms=['HS256'])
 
-    # return credentials
-    return ""
+    # message should be equivalent
+    return message == decoded_message
+
 
 
 if __name__ == '__main__':
 
-    credentials = authenticate_key()
+    if authenticate_key() == True:
+        print("server authenticated")
+    else:
+        print("server is adversary")
+
 
     # sending get request and saving the response as response object 
     r = requests.get(url = URL)
--- server.py
+++ server.py
@@ -2,24 +2,34 @@ from flask import Flask
 import jwt
 app = Flask(__name__)
 
-
+# test route
 @app.route('/')
 def hello_world():
     return 'here is your data'
 
-
 def authenticate_client_key():
+
+    # Receive key from MS ADAL
+    key = ""
 
     # Decode key once received from AD 
     jwt.decode(key, 'secret', algorithms=['HS256'])
 
     # return credentials
-    return ""
+    return True
+
+@app.route('/generate_key_pairs')
+def generate_key_pairs():
+    print("calling issuer for key pairs")
+
+    # Call issuer
+
 
 # Key route
-@app.route('??')
-    authenticate_client_key()
-    return
+@app.route('/secure')
+def process_request():
+    return "here is data"
+
 
 if __name__ == '__main__':
     app.run()
