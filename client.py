f65e05bb5e081ea529193c94a404e7f7addce403
Jeff Hensal <hensal.7@osu.edu>
2019-10-20 23:49:48 -0400

    Adding minimal bearer token authentication

--- client.py
+++ client.py
@@ -1,17 +1,29 @@
 # jeff
 # importing the requests library 
 import requests 
+import jwt 
 
-# api-endpoint 
-URL = "http://127.0.0.1:5000/"
+def createJwt():
+	encodedJwt = jwt.encode({'TestSecret': 'TestPassword'}, 'secret', algorithm='HS256')
+	return encodedJwt 
 
-# sending get request and saving the response as response object 
-r = requests.get(url = URL)
+def sendRequest(encodedJwt):
+	# sending get request and saving the response as response object 
+	r = requests.get(url = URL,  headers={'Auth': encodedJwt})
+	
+	if r == 200:
+		print('Success!')
+	elif r == 404:
+		print('Not Found.')
 
-if r == 200:
-    print('Success!')
-elif r == 404:
-    print('Not Found.')
+	# printing the output 
+	print("Response: %s"%r.text)
 
-# printing the output 
-print("Response: %s"%r.text)
+if __name__ == '__main__':
+	
+	# local api-endpoint 
+	URL = "http://127.0.0.1:5000/"
+	
+	sendRequest(createJwt())
+	
+	
--- server.py
+++ server.py
@@ -1,10 +1,19 @@
-from flask import Flask
+import jwt
+import json
+from flask import Flask, request
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'here is your data'
-
+	headers = request.headers
+	decodedJwt = jwt.decode(headers.get('Auth'), 'secret', algorithms=['HS256'])
+	 	
+	if decodedJwt['TestSecret'] == 'TestPassword':
+		return 'Authorized, here is your data!'
+	else:
+		return 'Unauthorized, who do you know here?' 
+	
+		
 if __name__ == '__main__':
     app.run()
